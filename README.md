419361c128b689ae0ea94e922509b444b77dc6c0
Xinran Li <xli73@u.rochester.edu>
2013-12-31 23:14:52 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+UR Balances for Firefox
+=======================
+
+Shows yoUR balances in Mozilla Firefox.
+
+Usage
+-----
+
+[Install the Mozilla Add-on SDK][sdk-inst] first.
+
+    cfx run
+
+License
+-------
+
+Apache License, Version 2.0.
+
+See also
+--------
+
+* [bb][bb]
+
+* [BetterBB][bbb]
+
+[bb]: https://github.com/RocHack/bb
+[bbb]: https://github.com/tylucaskelley/BetterBB
+[sdk-inst]: https://developer.mozilla.org/en-US/Add-ons/SDK/Tutorials/Installation
---
+++ data/panel.js
@@ -0,0 +1,63 @@
+'use strict';
+
+function refresh() {
+    addon.port.emit('refresh');
+}
+
+function logout() {
+    addon.port.emit('logout');
+}
+
+function passLogin(e) {
+    // Prevents form submission.
+    e.preventDefault();
+    var login = {
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value
+    };
+    // Passes the login to the main add-on module.
+    addon.port.emit('login-obtained', login);
+    // Prevents form submission.
+    return false;
+}
+
+addon.port.on('show-balances', function (balances) {
+    document.getElementById('declining').innerHTML = balances.declining;
+    document.getElementById('uros').innerHTML = balances.uros;
+    document.getElementById('balances').style.display = 'block';
+});
+
+addon.port.on('hide-balances', function () {
+    document.getElementById('declining').innerHTML = '';
+    document.getElementById('uros').innerHTML = '';
+    document.getElementById('balances').style.display = 'none';
+});
+
+addon.port.on('login-completed', function () {
+    document.getElementById('username').value = '';
+    document.getElementById('password').value = '';
+});
+
+addon.port.on('show-form', function () {
+    document.getElementById('login-form').style.display = 'block';
+});
+
+addon.port.on('hide-form', function () {
+    document.getElementById('login-form').style.display = 'none';
+});
+
+addon.port.on('reset-password', function () {
+    document.getElementById('password').value = '';
+    document.getElementById('password').focus();
+});
+
+window.onload = function () {
+    // Enables the buttons.
+    document.getElementById('login-form')
+            .addEventListener('submit', passLogin, false);
+    document.getElementById('refresh')
+            .addEventListener('mouseup', refresh, false);
+    document.getElementById('logout')
+            .addEventListener('mouseup', logout, false);
+    addon.port.emit('panel-loaded');
+};
